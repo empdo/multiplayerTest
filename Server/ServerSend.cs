29e14d5bd45a0fd9dce57f46307e6db2bb172ccc
Emil Essung <emil.essung@live.se>
2021-07-07 13:56:15 +0200

    add serverSendScript

---
+++ Server/ServerSend.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace GameServer {
+	class ServerSend {
+		public static void InitPacket(int id) {
+
+		}
+
+		public static void SendPacketToAll(int localPlayer, Packet packet) {
+
+		}
+
+		public static void Disconect(int id) {
+
+		}
+	}
+
+}
